64ed6b12697d6d83f8653ef03ae283d601c38231
Pedro L.C.A. <thezakman@icloud.com>
2015-05-27 15:50:08 -0300

    Update MD5-DICT.py
    fixed some issue

--- MD5-DICT.py
+++ MD5-DICT.py
@@ -7,16 +7,17 @@ print "[Md5 Dict-Cracker]"
 print "|  wordlist.txt  |\n"
 crackme = raw_input("MD5:")
 
+# Carrega o dict rockyou no local default do kali
 #f = open('wordlist.txt', 'r')
 f = open('/usr/share/wordlists/rockyou.txt', 'r')
 
+#Quebra linha por linha
 words = [line[0:-1] for line in f.readlines()]
 
-
-#words = 'test', 'alex', 'steve', 'admin'
+#[debug]
 #print words
 
-
+# Compara cada valor
 for word in words:
 	if md5(word).hexdigest() == crackme:
 		print "\nCracked:", word
